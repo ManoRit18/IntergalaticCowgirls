29308bfc54d09587ad9f87d254ff2033de8228b5
Kaya Bramble <kayabramble@gmail.com>
2020-10-03 13:04:18 -0700

    Shiny + different icons for categories

--- Map.R
+++ Map.R
@@ -4,5 +4,7 @@ library(leaflet)
 leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
 
 m <- leaflet() %>% setView(lng = -120.7401, lat = 47.7511, zoom = 6.4)
-m %>% addTiles()
+m %>% addProviderTiles(providers$CartoDB.Positron)
+
+
 
