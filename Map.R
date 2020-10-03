5da0cd73514e69454bb017513647467af9fffad5
Kaya Bramble <kayabramble@gmail.com>
2020-10-03 16:09:01 -0700

    rocket PNGS and new icons (need to add third icon option)

--- Map.R
+++ Map.R
@@ -1,10 +1,41 @@
 library(leaflet)
+library(shiny)
+
+wa_locations = read.csv("wa_locations.csv")
 
 # Set value for the minZoom and maxZoom settings.
 leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
 
-m <- leaflet() %>% setView(lng = -120.7401, lat = 47.7511, zoom = 6.4)
-m %>% addProviderTiles(providers$CartoDB.Positron)
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
 
+ui <- fluidPage(
+  titlePanel("Space Exploration in Washington"),
+  leafletOutput("mymap"),
+  p(),
+)
 
+server <- function(input, output, session) {
+  
+  points <- eventReactive(input$recalc, {
+    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+  }, ignoreNULL = FALSE)
+  
+  output$mymap <- renderLeaflet({
 
+    m <- leaflet() %>% setView(lng = -120.7401, lat = 47.7511, zoom = 6)
+    m %>% addProviderTiles(providers$CartoDB.Positron)
+    
+    rocketIcons <- icons(
+      iconUrl = ifelse(wa_locations$Category == "Business",
+        "Blue rocket.jpg",
+        "redrocket.png"),
+      iconWidth = 38, iconHeight = 90,
+      iconAnchorX = 22, iconAnchorY = 94,)
+    
+    leaflet(data = wa_locations) %>% addTiles() %>%
+      addMarkers(~-Longitude, ~Latitude, popup = ~as.character(Name), icon=rocketIcons)
+  })
+}
+
+shinyApp(ui, server)
