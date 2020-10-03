a8188fa30302f8b6003408803d576997ec3402f6
Kaya Bramble <kayabramble@gmail.com>
2020-10-03 11:48:31 -0700

    Map of washington

---
+++ Map.R
@@ -0,0 +1,8 @@
+library(leaflet)
+
+# Set value for the minZoom and maxZoom settings.
+leaflet(options = leafletOptions(minZoom = 0, maxZoom = 18))
+
+m <- leaflet() %>% setView(lng = -120.7401, lat = 47.7511, zoom = 6.4)
+m %>% addTiles()
+
