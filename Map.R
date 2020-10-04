b5fcbeebd312aeb0d6642b26db797e5ddd47fa7d
Kaya Bramble <kayabramble@gmail.com>
2020-10-04 11:09:12 -0700

    New pngs and adding sidebar panel and descriptions for app

--- Map.R
+++ Map.R
@@ -11,7 +11,39 @@ names(r_colors) <- colors()
 
 ui <- fluidPage(
   titlePanel("Space Exploration in Washington"),
-  leafletOutput("mymap"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      column(3, 
+             checkboxGroupInput("checkGroup", 
+                                h3("Checkbox group"), 
+                                choices = c("Business" = 1, 
+                                               "Academic" = 2, 
+                                               "Recreational" = 3),
+                                selected = c(1, 2, 3))),
+    ),
+    mainPanel(
+      leafletOutput("mymap"),
+      
+      h2("Local Events"),
+      
+      h2("Recent News"),
+      
+      h2("Notable Washingtonians"),
+      p("Thora Waters Halstead: Space biologist (WSU '50)"),
+      p("Jennifer Ross-Nazzal: NASA Historian (WSU '04)"),
+      p("Iris Fujiura Bombelyn: Engineer (WSU '83)"),
+      tags$a(href="https://magazine.wsu.edu/web-extra/space-cougs/", 
+             "More WSU graduates who have contributed to space exploration"),
+      p("Dottie Metcalf-Lindenburger: Astronaut"),
+      p("Wendy Lawrence: Astronaut"),
+      tags$a(href="https://www.seattletimes.com/seattle-news/5-washington-astronauts-try-to-put-outer-space-into-words-grandeur-that-is-beyond-what-i-can-describe/", 
+             "5 astronauts from Washington State"),
+      h2("About")
+    ),
+    
+  ),
+
   p(),
 )
 
@@ -28,10 +60,12 @@ server <- function(input, output, session) {
     
     rocketIcons <- icons(
       iconUrl = ifelse(wa_locations$Category == "Business",
-        "greenrocket.jpg",
-        "redrocket.png"),
+                       "greenrocket.png",
+                       ifelse(wa_locations$Category == "Academic",
+                       "redrocket.png",
+                       "bluerocket.png")),
       iconWidth = 38, iconHeight = 90,
-      iconAnchorX = 22, iconAnchorY = 94,)
+      iconAnchorX = 22, iconAnchorY = 40,)
     
     leaflet(data = wa_locations) %>% addTiles() %>%
       addMarkers(~-Longitude, ~Latitude, popup = ~as.character(Name), icon=rocketIcons)
